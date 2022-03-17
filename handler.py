af56f15ba849b514b9c01faa38132755fb9f5dce
localleon <me@lrau.xyz>
2022-03-17 07:31:51 +0100

    Added file storage, pip3 packages added

--- handler.py
+++ handler.py
@@ -1,10 +1,16 @@
 import json
 import io
 import boto3
+import datetime
+
+# Config Option -> migrate to env variables
+bucket_name = "hugo-cms-store1"
+s3 = boto3.client("s3")
 
 
 def upload_post(event, context):
-    # upload_post handles the uploading process of the api
+    """Upload_post handles the uploading process of the api"""
+
     post_params = json.loads(event["body"])
 
     # Check if request is valid
@@ -14,35 +20,41 @@ def upload_post(event, context):
 
     # Create an in-memory file and write to aws s3
     post_file = create_post_file(params=post_params)
+    write_to_s3(file=post_file, filename=get_filename_from_post(post_params["title"]))
 
     # We created the post in the s3 bucket
     response = {"statusCode": 200, "body": "Post was created successfully"}
     return response
 
 
-def write_to_s3(file, path):
-    s3 = boto3.client("s3")
+def get_filename_from_post(title):
+    """Converts the titel to a filename (cut length at 20) and adds a timestamp"""
+    short_title = title.replace(" ", "_")[:20]
+    return f"{datetime.date.today().isoformat()}_{short_title}.md"
+
+
+def write_to_s3(file, filename, path=None):
+    print(f"Writing to {bucket_name} with {filename} in {path}")
+    # construct the s3_key based on if the file should be stored in a folder
+    s3_path = path + "/" + filename if path != None else filename
+
+    s3.upload_fileobj(file, bucket_name, s3_path)
 
 
 def create_post_file(params):
-    md_file = io.StringIO()
-    # metadata section
-    md_file.write("---\n")
-    md_file.write(f"title: {params['title']}\n")
-    md_file.write(f"date: {params['date']}\n")
-    md_file.write(f"author: {params['author']}\n")
-    md_file.write("draft: false\n")
+    """Create_post_file builds a virtual file in memory and returns it"""
+    metadata = f"---\ntitle: {params['title']}\ndate: {params['date']}\nauthor: {params['author']}\ndraft: false\n"
+    content = f"---\n{params['content']}\n---\n"
 
-    # Content section
-    md_file.write("---\n")
-    md_file.write(f'{params["content"]}\n')
-    md_file.write("---\n")
+    # content of markdown needs to be encoded into bytes
+    md_file = io.BytesIO()
+    md_file.write((metadata + content).encode("UTF-8"))
 
     md_file.seek(0)
     return md_file
 
 
 def validate_params(params):
-    # validate_params takes the body of the requests and checks if every valid parameter for an upload is filled in
+    """Tkes the body of the requests and checks if every valid parameter for an upload is filled in"""
     required_keys = ("title", "date", "author", "content")
     return all(keys in params for keys in required_keys)
--- test.md
+++
@@ -1,8 +0,0 @@
----
-title: $ blog init
-date: 2019-03-19T10:47:00+01:00
-author: Leon Rauschenbach
-draft: false
----
-Dies ist ein TEstcontent.  has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially un 
----
