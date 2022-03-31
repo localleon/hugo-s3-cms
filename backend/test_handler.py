d2d42bed3c0c941609f6ba149980286c83800217
Leon <me@lrau.xyz>
2022-03-31 14:35:58 +0200

    Improved CI - Unit Testing works with Mocking (#1)
    * Dev requirements
    * Unit-Tests -v
    * Implemented s3 mocking unit-tests

--- backend/test_handler.py
+++ backend/test_handler.py
@@ -1,10 +1,8 @@
 # test_handler.py
-from asyncio import events
-from calendar import c
-import boto3
-import pytest
 import handler
 import json
+from moto import mock_s3
+import boto3
 
 
 def load_sample_event(path):
@@ -13,8 +11,11 @@ def load_sample_event(path):
         return json.load(ef)
 
 
-## Test Setup Functions
+############# Helper/Calc Function Testing #############
+
+
 def test_callback():
+    """Check if the AWS Event Callback function works properly"""
     testBody = {"msg": "Error1", "Content": "Content1"}
 
     # Check correct callback http code
@@ -53,10 +54,10 @@ def test_validate_post_json():
 
 
 def test_get_filename_from_post():
+    """Check valid and invalide titles for correct filename parsing"""
     valid_titles = ["Kuchen To-Go", "EinWortSpiel111"]
     invalid_titles = ["#HackingFest____!!!", "    !!!!    "]
 
-    # Check valid and invalide titles for correct filename parsing
     assert all([handler.get_filename_from_post(t) is not None for t in valid_titles])
     assert not all(
         [handler.get_filename_from_post(t) is not None for t in invalid_titles]
@@ -64,10 +65,10 @@ def test_get_filename_from_post():
 
 
 def test_get_body_from_event():
+    """Check if our helper funciton can successfully load aws events"""
     event = load_sample_event("sample_events/upload.json")
     body = handler.get_body_from_event(event)
 
-    # Check if our helper funciton can successfully load aws events
     assert body["title"] == "SpecialChars"
     assert body["author"] == "Rauschenberg"
     assert not len(body["content"]) == 0
@@ -76,19 +77,59 @@ def test_get_body_from_event():
 
 
 def test_calc_paging_index():
+    """Test paging function for correctly calculated indexes"""
     # Correct pagging results for the first 3 pages
     page_tests = [(1, (0, 7)), (2, (8, 15)), (3, (16, 23)), (99, (784, 791))]
 
-    # Test paging function for correctly calculated indexes
     assert all([handler.calc_paging_index(test[0]) == test[1] for test in page_tests])
 
 
+############# AWS S3 Mocking and Testing #############
+def mock_s3_ressources():
+    """This methode emulates"""
+    print("mocking s3 infra.....")
+    handler.init_s3()
+
+    handler.s3.create_bucket(
+        Bucket=handler.bucket_name,
+        CreateBucketConfiguration={"LocationConstraint": "eu-central-1"},
+    )
+
+    # put 50 random markdown files
+    data = "Hello from mocking file!".encode("UTF-8")
+    for x in range(1, 50):
+        filename = f"MockFile-{x}.md"
+        handler.s3.put_object(
+            Body=data,
+            Bucket=handler.bucket_name,
+            Key=filename,
+        )
+    # put one illegal file
+    handler.s3.put_object(
+        Body=data,
+        Bucket=handler.bucket_name,
+        Key="ILLEGAL_FILE.txt",
+    )
+
+
 def test_list_objects_from_bucket_paged_size_only():
-    # Test first five pages for the correct size of items
-    pages = [len(handler.list_objects_from_bucket_paged(p)) <= 8 for p in range(1, 5)]
-    assert all(pages)
+    """Check if only 8 Items are returned per s3 obejct page"""
+    with mock_s3():
+        mock_s3_ressources()
+        # Test first five pages for the correct size of items
+        pages = [
+            len(handler.list_objects_from_bucket_paged(p)) <= 8 for p in range(1, 5)
+        ]
+        assert all(pages)
 
 
-def test_list_objects_from_bucket_paged():
-    # TODO: Check if we return the correct keys from mock s3
-    assert True
+def test_list_objects_from_bucket():
+    """Mock_s3 and check if we can successfully access all .md Items in our bucket"""
+    with mock_s3():
+        mock_s3_ressources()
+        objs = handler.list_objects_from_bucket()
+
+        file_exists = [f"MockFile-{x}.md" in objs for x in range(1, 8)]
+
+        assert all(file_exists)
+        assert "ILLEGAL_FILE.txt" not in objs
